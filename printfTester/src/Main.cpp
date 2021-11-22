543d055e41c9d0c6cfc21e4a67c40fd669106620
Daan Meijer <daansander02@outlook.com>
2021-11-22 11:12:08 +0100

    Update to latest version of Air-Engine

--- printfTester/src/Main.cpp
+++ printfTester/src/Main.cpp
@@ -1,26 +1,13 @@
 #include <iostream>
+#include <unistd.h>
 #include "AirTester.h"
 
 extern "C" {
 #include "ft_printf.h"
 }
 
-TEST(mandatory_tests, character) {
-	char correct_buffer[1024];
-	char test_buffer[1024];
-
-	std::sprintf(correct_buffer, "There are no chars in this strings!\n");
-
-	EXPECT_TRUE(true == true)
-	EXPECT_TRUE(false == true)
-	EXPECT_FALSE(false == false)
-	EXPECT_FALSE(true == false)
-	EXPECT_EQUAL(5, 6)
-	EXPECT_EQUAL(5, 5)
-	EXPECT_EQUAL("Abc", "Abc")
-	EXPECT_EQUAL("Abc", "abc")
-	EXPECT_NOT_EQUAL("Abc", "Abc")
-	EXPECT_NOT_EQUAL("Abc", "abc")
+TEST(mandatory_tests, no_arg) {
+	
 }
 
 int main(int argc, char **argv) {
