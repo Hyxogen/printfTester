fdb688b2be8387b2302f4b2ad9445934768f271a
Daan Meijer <daansander02@outlook.com>
2021-11-23 10:38:18 +0100

    Make makefile working

--- src/tests/CharTests.cpp
+++ src/tests/CharTests.cpp
@@ -1,15 +1,23 @@
 int	SingleCharTest(int (*printf1)(const char *, ...), int (*printf2)(const char *, ...)) {
+	(void)printf1;
+	(void)printf2;
 	return (true);
 }
 
 int CharInString(int (*printf1)(const char *, ...), int (*printf2)(const char *, ...)) {
+	(void)printf1;
+	(void)printf2;
 	return (true);
 }
 
 int CharsInString(int (*printf1)(const char *, ...), int (*printf2)(const char *, ...)) {
+	(void)printf1;
+	(void)printf2;
 	return (true);
 }
 
 int OnlyCharsString(int (*printf1)(const char *, ...), int (*printf2)(const char *, ...)) {
+	(void)printf1;
+	(void)printf2;
 	return (true);
 }
