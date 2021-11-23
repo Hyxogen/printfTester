6ff311b3f9e584ed9bfe031b5c3973ac0daf02bc
Daan Meijer <daansander02@outlook.com>
2021-11-23 10:35:42 +0100

    Refactor tester

--- src/Tester.cpp
+++ src/Tester.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "tests/Tests.h"
 
 static int (*g_tests[])(int (*printf1)(const char *, ...), int (*printf2)(const char *, ...)) = {
@@ -5,19 +6,23 @@ static int (*g_tests[])(int (*printf1)(const char *, ...), int (*printf2)(const 
 		0
 };
 
-static int run_all_tests() {
+static int RunAllTests(int (*printf1)(const char *, ...), int (*printf2)(const char *, ...)) {
 	int index;
 	int ret;
 
 	index  = 0;
 	ret = 1;
 	while (g_tests[index])
-		ret *= g_tests[index++]();
+		ret *= g_tests[index++](printf1, printf2);
 	return ret;
+}
+
+static int RunAllTests() {
+	return RunAllTests(&std::printf, &std::printf);
 }
 
 int main(int argc, char **argv) {
 	(void)argc;
 	(void)argv;
-	return !run_all_tests();
+	return !RunAllTests();
 }
